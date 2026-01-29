74f82a0527d7d4b8e9a5e1fb8850f355e8e066da
Inacio Vasquez <inaciovasquez2020@gmail.com>
2026-01-28 22:53:19 -0300

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -5,8 +5,7 @@ package chronos_urf_rr where
   moreServerArgs := #["-K", "1024"]
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.27.0"
 
 require urf_core from git
   "https://github.com/inaciovasquez2020/urf-core.git" @ "main"
-
