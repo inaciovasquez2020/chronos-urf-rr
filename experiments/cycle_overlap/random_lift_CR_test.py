ae4d729d08f9ca0320b839c7274ae3c648e807d1
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-04 11:26:36 -0600

    Replace networkx lift with explicit random k-lift implementation

--- experiments/cycle_overlap/random_lift_CR_test.py
+++ experiments/cycle_overlap/random_lift_CR_test.py
@@ -1,13 +1,32 @@
 # experiments/cycle_overlap/random_lift_CR_test.py
 
 import networkx as nx
+import random
 from compute_CR import compute_CR
+
 
 def base_graph():
     return nx.complete_graph(4)
 
-def random_lift(base, n):
-    return nx.algorithms.lifted_graph.lift(base, n)
+
+def random_lift(base, k):
+    """
+    k-lift of base graph using random permutations.
+    """
+    G = nx.Graph()
+
+    for v in base.nodes():
+        for i in range(k):
+            G.add_node((v, i))
+
+    for (u, v) in base.edges():
+        perm = list(range(k))
+        random.shuffle(perm)
+        for i in range(k):
+            G.add_edge((u, i), (v, perm[i]))
+
+    return nx.convert_node_labels_to_integers(G)
+
 
 def run():
     R = 2
@@ -15,10 +34,11 @@ def run():
 
     print("\n=== RANDOM LIFT TEST ===")
 
-    for n in [10, 20, 40, 80, 160]:
-        G = random_lift(base, n)
+    for k in [10, 20, 40, 80]:
+        G = random_lift(base, k)
         CR = compute_CR(G, R)
-        print("lift n=", n, "CR=", CR)
+        print("lift size=", k, "CR=", CR)
+
 
 if __name__ == "__main__":
     run()
