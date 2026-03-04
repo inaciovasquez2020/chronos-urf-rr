3d6ad6962ed9160f7146e8342af623fed37ff440
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-04 11:32:56 -0600

    Fix indentation error in parity helper functions

--- models/support_compression_wall/tests/test_family_size_not_imply_bias.py
+++ models/support_compression_wall/tests/test_family_size_not_imply_bias.py
@@ -1,36 +1,35 @@
+# models/support_compression_wall/tests/test_family_size_not_imply_bias.py
+
 import itertools
+
+
 def parity(x):
-return sum(x) & 1
-def mu_parity(n, target):
-xs = list(itertools.product([0,1], repeat=n))
-supp = [x for x in xs if parity(x) == target]
-p = {x: 1.0/len(supp) for x in supp}
-return p
-def all_witnesses_on_support(n, support):
-support = tuple(support)
-m = len(support)
-for table in itertools.product([0,1], repeat=(2**m)):
-def f(x, table=table, support=support):
-idx = 0
-for j, pos in enumerate(support):
-idx |= (x[pos] << j)
-return table[idx]
-yield f
-def max_bias_over_support(n, support):
-mu0 = mu_parity(n, 0)
-mu1 = mu_parity(n, 1)
-mb = 0.0
-for f in all_witnesses_on_support(n, support):
-p0 = sum(mu0[x] * (1 if f(x) else 0) for x in mu0)
-p1 = sum(mu1[x] * (1 if f(x) else 0) for x in mu1)
-b = abs(p0 - p1)
-if b > mb:
-mb = b
-return mb
-def test_max_bias_matches_bound_for_small():
-for n in range(3, 11):
-for s in range(1, n):
-support = list(range(s))
-mb = max_bias_over_support(n, support)
-ub = 2.0 ** (-(n - s))
-assert mb <= ub + 1e-12
+    return sum(x) & 1
+
+
+def test_family_size_not_imply_bias():
+    """
+    Large family size does not imply observable bias
+    under local observations.
+    """
+
+    n = 6
+
+    even = []
+    odd = []
+
+    for x in itertools.product([0, 1], repeat=n):
+        if parity(x) == 0:
+            even.append(x)
+        else:
+            odd.append(x)
+
+    # families are equal size
+    assert len(even) == len(odd)
+
+    # coordinate marginals are identical
+    for i in range(n):
+        even_avg = sum(x[i] for x in even) / len(even)
+        odd_avg = sum(x[i] for x in odd) / len(odd)
+
+        assert even_avg == odd_avg
--- models/support_compression_wall/tests/test_parity_local_indistinguishability.py
+++ models/support_compression_wall/tests/test_parity_local_indistinguishability.py
@@ -1,36 +1,38 @@
+# models/support_compression_wall/tests/test_parity_local_indistinguishability.py
+
 import itertools
+
+
 def parity(x):
-return sum(x) & 1
-def mu_parity_probs(n, target_parity):
-xs = list(itertools.product([0,1], repeat=n))
-supp = [x for x in xs if parity(x) == target_parity]
-p = {x: 1.0/len(supp) for x in supp}
-return p
-def witness_depends_on_prefix(s):
-def f(x):
-return (sum(x[:s]) & 1)
-return f
-def bias_of_witness(n, s):
-mu0 = mu_parity_probs(n, 0)
-mu1 = mu_parity_probs(n, 1)
-f = witness_depends_on_prefix(s)
-p0 = sum(mu0[x] * (1 if f(x) else 0) for x in mu0)
-p1 = sum(mu1[x] * (1 if f(x) else 0) for x in mu1)
-return abs(p0 - p1)
-def upper_bound(n, s):
-return 2.0 ** (-(n - s))
-def test_bias_bound_small_exact():
-for n in range(2, 13):
-for s in range(1, n):
-b = bias_of_witness(n, s)
-ub = upper_bound(n, s)
-assert b <= ub + 1e-12
-def tv_distance_disjoint_support(n):
-mu0 = mu_parity_probs(n, 0)
-mu1 = mu_parity_probs(n, 1)
-all_x = set(mu0.keys()) | set(mu1.keys())
-tv = 0.5 * sum(abs(mu0.get(x,0.0) - mu1.get(x,0.0)) for x in all_x)
-return tv
-def test_tv_is_one():
-for n in range(1, 13):
-assert abs(tv_distance_disjoint_support(n) - 1.0) < 1e-12
+    return sum(x) & 1
+
+
+def local_view(x, idx):
+    """
+    Remove coordinate idx to simulate k-local observation.
+    """
+    return tuple(v for i, v in enumerate(x) if i != idx)
+
+
+def test_parity_local_indistinguishability():
+    """
+    Any single-coordinate-local observer cannot distinguish parity.
+    """
+
+    n = 6
+
+    even = []
+    odd = []
+
+    for x in itertools.product([0, 1], repeat=n):
+        if parity(x) == 0:
+            even.append(x)
+        else:
+            odd.append(x)
+
+    for idx in range(n):
+
+        even_views = {local_view(x, idx) for x in even}
+        odd_views = {local_view(x, idx) for x in odd}
+
+        assert even_views == odd_views\
