a933c982f6436b60baeb1a0eb73da3ab81d99eb7
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-10 07:45:27 -0500

    Fix lakefile: single Chronos library declaration

--- lakefile.lean
+++ lakefile.lean
@@ -1,10 +1,7 @@
 import Lake
 open Lake DSL
 
-package chronos_urf_rr
+package chronos
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"
-
-lean_lib Oblivion where
+lean_lib Chronos where
   srcDir := "lean"
