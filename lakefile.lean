46bb55199de41b973870b2fc31750f78671bc23a
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-31 13:25:49 -0300

    Align mathlib+batteries with Lean 4.28 (resolve binder annotation failure)

--- lakefile.lean
+++ lakefile.lean
@@ -2,8 +2,8 @@ import Lake
 open Lake DSL
 package chronos_urf_rr where
 require mathlib from git
-"https://github.com/leanprover-community/mathlib4" @ "master"
+"https://github.com/leanprover-community/mathlib4" @ "69cbc416b3f58ba11e331db067644d0508e78341"
 require batteries from git
-"https://github.com/inaciovasquez2020/batteries-fixed" @ "main"
+"https://github.com/leanprover-community/batteries" @ "main"
 require urf_core from git
 "https://github.com/inaciovasquez2020/urf-core.git" @ "main"
