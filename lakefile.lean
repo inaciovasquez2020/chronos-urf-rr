dd28d6e940350ec4c660536445294e68346b57d5
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-31 13:54:05 -0300

    Fix CI: remove direct deps, let mathlib control dependency graph (resolve cache + mismatch error)

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,11 @@
 import Lake
 open Lake DSL
+
 package chronos_urf_rr where
-require mathlib from git
-"https://github.com/leanprover-community/mathlib4" @ "69cbc416b3f58ba11e331db067644d0508e78341"
-require batteries from git
-"https://github.com/leanprover-community/batteries" @ "main"
+
 require urf_core from git
 "https://github.com/inaciovasquez2020/urf-core.git" @ "main"
+
+require mathlib from git
+"https://github.com/leanprover-community/mathlib4" @ "69cbc416b3f58ba11e331db067644d0508e78341"
+
