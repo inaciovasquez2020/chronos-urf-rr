217d74eaedac634eff352607e973f3f5cc741575
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-31 12:55:33 -0300

    Pin Batteries to patched fork (binder annotation fix)

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,9 @@
 import Lake
 open Lake DSL
-
-package «chronos_urf_rr» where
-
-@[default_target]
-lean_lib ChronosUrfRr where
-  srcDir := "lean"
-
+package chronos_urf_rr where
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"
-
+"https://github.com/leanprover-community/mathlib4" @ "master"
+require batteries from git
+"https://github.com/inaciovasquez2020/batteries-fixed" @ "main"
 require urf_core from git
-  "https://github.com/inaciovasquez2020/urf-core.git" @ "main"
+"https://github.com/inaciovasquez2020/urf-core.git" @ "main"
