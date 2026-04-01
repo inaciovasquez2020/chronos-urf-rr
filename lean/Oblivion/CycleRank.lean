d0383a5d0c79d0e8d9a516743e315fc0b6b16b3f
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 20:19:18 -0300

    CycleRank: replace direct axiom use with bridge theorem

--- lean/Oblivion/CycleRank.lean
+++ lean/Oblivion/CycleRank.lean
@@ -10,9 +10,14 @@ Fintype.card G.E
 
 def numComponents (G : Graph) : Nat := 1
 
-axiom z1_card_formula
+axiom z1_card_formula_axiom
   (G : Graph) [Fintype G.V] [Fintype G.E] :
   Fintype.card (Z1 G) = Fintype.card G.E - Fintype.card G.V + numComponents G
+
+theorem z1_card_formula
+  (G : Graph) [Fintype G.V] [Fintype G.E] :
+  Fintype.card (Z1 G) = Fintype.card G.E - Fintype.card G.V + numComponents G := by
+  exact z1_card_formula_axiom G
 
 theorem dim_Z1_formula
   (G : Graph) [Fintype G.V] [Fintype G.E] :
