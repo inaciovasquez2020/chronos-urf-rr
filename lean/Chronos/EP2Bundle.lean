2d2290b5d0903b7d8b7caf379c9971c78d32f1dc
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-05 12:14:33 -0300

    Rebuild EP2 bundle against unified Chronos scaffolds

---
+++ lean/Chronos/EP2Bundle.lean
@@ -0,0 +1,15 @@
+import Mathlib
+import Chronos.Z1
+import Chronos.ParityPair
+import Chronos.Transcript
+import Chronos.CloseSeeded
+import Chronos.ParitySep
+import Chronos.ParitySepProof
+import Chronos.EP2Status
+
+namespace Chronos
+
+theorem EP2_bundle_compiles : True := by
+  trivial
+
+end Chronos
