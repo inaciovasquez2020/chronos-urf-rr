5975623e90196833a124c9c53e8e921774c509f3
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-07-16 14:01:15 -0300

    lean: repair Quznor line 284 sequence focus (#1145)

--- lean/Chronos/Frontier/QuznorD1D5ThreeModeVandermondeInversion.lean
+++ lean/Chronos/Frontier/QuznorD1D5ThreeModeVandermondeInversion.lean
@@ -281,7 +281,7 @@ theorem quznorThreeModeEulerModewise_hasDerivAt_one
         (-3 * S3)
         1
         (by norm_num)
-      using 1 <;>
+      using 1 ;
         norm_num <;>
         ring_nf
 
